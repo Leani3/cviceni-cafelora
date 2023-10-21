108f77baaec1d0067622a2155c542ce70d24aa95
Líza P <liza.povkh@gmail.com>
2023-10-21 12:11:54 +0200

    4. Nápoj jako komponenta

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,12 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
-import { Header } from '../components/header';
-import { Banner } from '../components/banner';
-import { Menu } from '../components/menu';
-import { Gallery } from '../components/gallery';
-import { Contact } from '../components/contact';
-import { Footer } from '../components/footer';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { Menu } from '../components/Menu';
+import { Gallery } from '../components/Gallery';
+import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
